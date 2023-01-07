ff579b7394f4dd0227f992136743cf4025e39b91
2sseul <iseul0626@gmail.com>
2023-01-08 01:30:03 +0900

    section5 complete

--- udemy_01/src/App.js
+++ udemy_01/src/App.js
@@ -4,7 +4,7 @@ import NewExpense from "./components/Expenses/NewExpense/NewExpense";
 
 const INITIAL_EXPENSES= [
   {
-    id:'e1',
+    id:'e1', 
     title:'Toilet Paper',
     amount: 94.12,
     date: new Date(2021, 2, 3),
