ca0b3883c687f3c52455019ade8172e8dfc79214
2sseul <iseul0626@gmail.com>
2023-01-08 19:28:34 +0900

    sections 6 -ing

--- udemy_02/src/components/CourseGoals/CourseInput/CourseInput.js
+++ udemy_02/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,76 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input{
+    border-color: red;
+    background-color: #ffd7d7;
+  }
+
+  &.invalid label{
+    color: red;
+  }
+`;
+
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
+  //입력된 값이 유효한지 확인해주는 formSubmitHandler
   const formSubmitHandler = event => {
     event.preventDefault();
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
+    //유효한지 확인은 onAddGoal이 실행되기 전에 해줘야한다.
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <FormControl  className={!isValid && 'invalid'}>
         <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+        <input type="text" onChange={goalInputChangeHandler} /> 
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
+//transparent: 투명색
+
 
 export default CourseInput;
--- udemy_02/src/components/UI/Button/Button.js
+++ udemy_02/src/components/UI/Button/Button.js
@@ -1,13 +1,42 @@
-import React from 'react';
+// import React from 'react';
+//얘는 지워줘도 된다. 왜냐면 여기서 더이상 JSX 를 사용하지 않기 때문.
 
-import './Button.css';
+//styled는 styled-components에서 임포트하는 객체이고 button메소드에 접근할 수 있다.
+import styled from 'styled-components';
 
-const Button = props => {
-  return (
-    <button type={props.type} className="button" onClick={props.onClick}>
-      {props.children}
-    </button>
-  );
-};
+//tagged template literals
+//button은 styled객체의 메소드
+//button() 괄호붙여서 호출하는 메소드 대신 백틱을 붙여서 호출한다.
+//백틱 안에 입력한 것이 button 메서드 안에 들어간다.
+//그리고 이 button메서드가 button 컴포넌트를 반환한다는 것.
+const Button = styled.button`
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:hover,
+  &:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`;
+
+
+// const Button = props => {
+//   return (
+//     <button type={props.type} className="button" onClick={props.onClick}>
+//       {props.children}
+//     </button>
+//   );
+// };
 
 export default Button;
