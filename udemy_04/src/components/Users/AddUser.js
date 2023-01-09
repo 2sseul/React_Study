602e5ed14c656d7d2dddd2884b54591360d6bf41
2sseul <iseul0626@gmail.com>
2023-01-09 18:00:41 +0900

    section 8 -ing

--- udemy_04/src/components/Users/AddUser.js
+++ udemy_04/src/components/Users/AddUser.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import './AddUser.css'
 import Button from '../UI/Button';
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
     const addUserHandler = event => {
@@ -11,13 +13,15 @@ const AddUser = (props) => {
 
     //htmlFor는 자바스크립트에서 label에 for의 속성을 할당하는 props의 이름.
     return(
-        <form onSubmit={addUserHandler}>
-            <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
-            <label htmlFor="age">Age(Years)</label>
-            <input id="age" type="number" />
-            <Button type="submit"/>
-        </form>
+        <Card>
+            <form onSubmit={addUserHandler}>
+                <label htmlFor="username">Username</label>
+                <input id="username" type="text" />
+                <label htmlFor="age">Age(Years)</label>
+                <input id="age" type="number" />
+                <Button type="submit"/>
+            </form>
+        </Card>
     )
 };
 
