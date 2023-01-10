ba9b71651ffb678e976e59e0f878d34eae769308
2sseul <iseul0626@gmail.com>
2023-01-10 15:44:33 +0900

    section 8 complete

--- udemy_04/src/components/Users/AddUser.js
+++ udemy_04/src/components/Users/AddUser.js
@@ -10,13 +10,22 @@ const AddUser = (props) => {
     //useState를 호출해서 초기시작상태를 디폴트로 정의 
     const [enteredUsername, setEnteredUsername] = useState('');
     const [enteredAge, setEnteredAge] = useState('');
+    const [error, setError] = useState();   //어차피 undefined
     const addUserHandler = event => {
         event.preventDefault();
         if(enteredUsername.trim().length === 0 || enteredAge.trim().length === 0 ){
+            setError({
+                title: 'Invalid Input',
+                message: 'Please enter a valid name and age (non-empty values)',
+            });
             return;
         }
         //enteredAge는 기본적으로 문자열이라, +를 앞에 붙여주면 확실한 숫자형
         if(+enteredAge < 1){
+            setError({
+                title: 'Invalid age',
+                message: 'Please enter a valid age (age > 0)',
+            });
             return;
         }
         props.onAddUser(enteredUsername, enteredAge);
@@ -33,10 +42,14 @@ const AddUser = (props) => {
         setEnteredAge(event.target.value);
     };
 
+    const errorHandler = () => {
+        setError(null);
+    };
+
     //htmlFor는 자바스크립트에서 label에 for의 속성을 할당하는 props의 이름.
     return(
         <div>
-            <ErrorModal />
+            { error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler}/>}
             <Card cssClass={classes.input}>
                 <form onSubmit={addUserHandler }>
                     <label htmlFor="username">Username</label>
