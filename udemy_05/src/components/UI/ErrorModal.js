e7057f240fe2c3d6054e973016a9eac914063cea
2sseul <iseul0626@gmail.com>
2023-01-11 17:32:00 +0900

    section9 complete

--- udemy_05/src/components/UI/ErrorModal.js
+++ udemy_05/src/components/UI/ErrorModal.js
@@ -1,25 +1,40 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 
 import Card from './Card';
 import Button from './Button';
 import classes from './ErrorModal.module.css';
 
+const Backdrop = props => {
+  return <div className={classes.backdrop} onClick={props.onConfirm} />
+};
+
+const ModalOverlay = props => {
+  return  <Card className={classes.modal}>
+  <header className={classes.header}>
+    <h2>{props.title}</h2>
+  </header>
+  <div className={classes.content}>
+    <p>{props.message}</p>
+  </div>
+  <footer className={classes.actions}>
+    <Button onClick={props.onConfirm}>Okay</Button>
+  </footer>
+</Card>
+};
+
+//ReactDom.createPortal(렌더링 할 수 있는 자식노드(조건: JSX코드여야함), document.getElemetById('실제 렌더링 되는 DOM주소'))
+//실제 렌더링되는 DOM주소는 public내부의 index.html에 만들어놓은 <div id="backdrop-root">를 말한다.(backdrop이 렌더링되었으면 하는 위치)
+//document.getElementById는 리액트와 전혀 상관이 없다. 단지 DOM노드의 위치에 접근하기 위해 브라우저에서 제공하는 API를 사용한 것일 뿐.
+//이 API를 사용하여 실제 DOM요소에 접근하는 것
+
 const ErrorModal = (props) => {
+  //35번째 줄에서  onConfirm을 사용하는 이유는 Backdrop함수 내에서 props.Confirm을 사용했기 떄문 ~
   return (
-    <div>
-      <div className={classes.backdrop} onClick={props.onConfirm} />
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.onConfirm}>Okay</Button>
-        </footer>
-      </Card>
-    </div>
+    <React.Fragment>
+      {ReactDom.createPortal(<Backdrop onConfirm={props.onConfirm}/>,document.getElementById('backdrop-root'))}
+      {ReactDom.createPortal(<ModalOverlay tilte={props.title} message={props.message} onConfirm={props.onConfirm}/>, document.getElementById('overlay-root'))}
+    </React.Fragment>
   );
 };
 
--- udemy_05/src/components/Users/AddUser.js
+++ udemy_05/src/components/Users/AddUser.js
@@ -1,43 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
-import Wrapper from '../UI/Helper/Wrapper';
 
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  //모든 리액트 훅과 마찬가지로 함수형 컴포넌트 내부에서만 사용 가능하다.
+  //초기화하려는 기본값이 필요하지만 여기서 중요하진 않음.
+  //여기서 중요한건 useRef();가 무엇을 반환하는가.
+  //왜냐하면 나중에 해당 ref와 작업할 수 있게 해주는 값을 반환해주기 때문임 ~
+  //즉 요소와 연결하여 해당 요소와 작업할 수 있게 해준다.
+
+  //초기값은 undefined. 기본값임. 쓸모는 없음.
+  //이제 리액트에 ref와 연결하고싶다고 알려줘야함.
+  //ref와 연결하려는 해당 요소에 가서 ref prop을 연결해주면 된다.
+  const nameInputRef = useRef();
+  //nameInputRef안에 있는 요소는 실제 DOM요소가 될 것.
+  //nameInputRef를 통해 출력되는 값은 항상 객체!
+  //그리고 항상 current 프롭을 가지고 있다.
+  //current prop은 그 ref가 연결된 실제 값을 가진다.
+  const ageInputRef = useRef();
+
+  //값을 가져오기 위해 더이상 state를 사용하지 않기 때문에 이제 state 없어도 된다 !
+  // const [enteredUsername, setEnteredUsername] = useState('');
+  // const [enteredAge, setEnteredAge] = useState('');
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    //state는 필요하지 않다. ref가 input과 연결이 되면서, nameInputRef의 current의 value값이 input안에 집어넣은 값과 연결되었기 때문.
+    //상태 변화를 감지하는 state가 불필요해짐.
+    const enteredName = nameInputRef.current.value;
+    const entereUserdAge = ageInputRef.current.value;
+
+    if (enteredName.trim().length === 0 || entereUserdAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+entereUserdAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
-
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    props.onAddUser(enteredName, entereUserdAge);
+    nameInputRef.current.value = '';
+    ageInputRef.current.value = '';
   };
 
   const errorHandler = () => {
@@ -45,7 +58,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Wrapper>
+    <React.Fragment>
       {error && (
         <ErrorModal
           title={error.title}
@@ -59,20 +72,18 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </Wrapper>
+    </React.Fragment>
   );
 };
 
