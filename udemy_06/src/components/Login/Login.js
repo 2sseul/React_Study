69c972bcabf98ffcd0804d83a7979cef3a4f1bf0
2sseul <iseul0626@gmail.com>
2023-01-16 02:59:15 +0900

    section 10 -ing

--- udemy_06/src/components/Login/Login.js
+++ udemy_06/src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
@@ -11,12 +11,22 @@ const Login = (props) => {
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  //이미 이메일, 비밀번호 유효성 체크는 유효하지만, useEffect를 사용해보자.
+  useEffect(()=>{
+    setFormIsValid(
+      //이메일에 @가 포함되어 있고, 입력된 비밀번호의 길이 > 6인 경우
+      enteredEmail.includes('@') && enteredPassword.trim().length > 6
+    );
+  },[setFormIsValid, enteredEmail, enteredPassword]);
+
+  //email 필드의 모든 키 입력에 대해 emailChangeHandler가 실행되어서
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
+    //form이 유효한지 체크
+    // setFormIsValid(
+    //   //이메일에 @가 포함되어 있고, 입력된 비밀번호의 길이 > 6인 경우
+    //   event.target.value.includes('@') && enteredPassword.trim().length > 6
+    // );
   };
 
   const passwordChangeHandler = (event) => {
