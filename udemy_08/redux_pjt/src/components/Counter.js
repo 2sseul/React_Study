cba2f537828ae7d09a6781fd8b1bf83c282349db
2sseul <iseul0626@gmail.com>
2023-01-31 13:46:30 +0900

    section 18 -ing

--- udemy_08/redux_pjt/src/components/Counter.js
+++ udemy_08/redux_pjt/src/components/Counter.js
@@ -1,6 +1,12 @@
-import classes from './Counter.module.css';
+import { useSelector } from "react-redux";
+//useSelector: 저장소가 관리하는 상태 부분을 자동으로 선택할 수 있다.
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  //저장소에서 추출하려는 데이터 부분 결정
+  const counter = useSelector((state) => state.counter);
+
   const toggleCounterHandler = () => {};
 
   return (
---
+++ udemy_08/redux_pjt/src/store/index.js
@@ -0,0 +1,20 @@
+//여기에 리덕스 코드 들어옴.
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
