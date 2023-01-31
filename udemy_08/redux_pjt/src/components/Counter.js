8e2548ced999972807bd5045ccf9c758cb315a4a
2sseul <iseul0626@gmail.com>
2023-01-31 18:01:47 +0900

    section 18 -ing

--- udemy_08/redux_pjt/src/components/Counter.js
+++ udemy_08/redux_pjt/src/components/Counter.js
@@ -1,18 +1,48 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 //useSelector: 저장소가 관리하는 상태 부분을 자동으로 선택할 수 있다.
 
 import classes from "./Counter.module.css";
 
 const Counter = () => {
+  //어떤 인자도 전달하지 않고 대신 실행할 수 있는 dispatch function
+  //redux store에 대한 action을 보낸다.
+  const dispatch = useDispatch();
   //저장소에서 추출하려는 데이터 부분 결정
   const counter = useSelector((state) => state.counter);
 
-  const toggleCounterHandler = () => {};
+  const show = useSelector((state) => state.showCounter);
+
+  const increaseHandler = () => {
+    dispatch({
+      type: "increase",
+      amount: 5,
+    });
+  };
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
+  //toggle로 숨기고 보여지게 하는 것은 useState사용해서 관리해야한다.
+  //왜냐믄요 이 컴포넌트에서만 쓰는거니까요...!(local state)
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+
+      {show && <div className={classes.value}>{counter}</div>}
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase by 5</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- udemy_08/redux_pjt/src/store/index.js
+++ udemy_08/redux_pjt/src/store/index.js
@@ -1,17 +1,31 @@
 //여기에 리덕스 코드 들어옴.
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
+const initialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initialState, action) => {
+  if (action.type === "increase") {
     return {
-      counter: state.counter + 1,
+      //action.amount: action payload
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
+
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
     };
   }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
+    };
+  }
+
   return state;
 };
 
