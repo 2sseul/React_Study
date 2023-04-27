cd9c5fb2c922c252e37c7669ffd7fb8f63a72e6f
2sseul <iseul0626@gmail.com>
2023-04-27 11:30:25 +0900

    section 22 -ing

--- udemy_10/src/basics.ts
+++ udemy_10/src/basics.ts
@@ -27,10 +27,14 @@ hobbies = ["Sports", "Cookking"];
 
 //하나의 구조만 가지는 객체를 저장하고 싶음
 //bool 타입을 가지는 person 객체는 저장하고 싶지 않음
-let person: {
+
+//타입 별칭 지정
+type Person = {
   name: string;
   age: number;
 };
+
+let person: Person;
 
 person = {
   name: "Max",
@@ -50,3 +54,18 @@ let people: {
 }[];
 
 //type inference(타입추론)
+
+let cousre = "React - The Complete Guide";
+
+// cousre = 12341;
+// 유니온 타입은 타입을 정의할 때 한 개 이상의 타입을 사용할 수 있다.
+// 첫번째 타입 뒤에 | 넣고 뒤에 다른 타입 추가하면 된다
+// let course string | number | boolean = 'React - The Complete Guide';
+// 유니온 타입은 타입을 지정한 곳이면 어디서든 사용 가능하다.
+// 타입추론을 사용하는 경우가 아니라면 !
+
+//Function & types
+
+function add(a: number, b: number) {
+  return a + b;
+}
